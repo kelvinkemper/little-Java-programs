9de718dc0242384fa93c035a32dc85c3ce6188bf
Kelvin Kemper <39568186+kelvinkemper@users.noreply.github.com>
2020-01-29 00:31:40 -0700

    Add files via upload

---
+++ multTable15x15.java
@@ -0,0 +1,12 @@
+public class multTable15x15 {
+
+    public static void main(String args[]) {
+
+        for (int i = 1; i <= 15; i ++) {
+            for (int j = 1; j <= 15; j++) {
+                System.out.print(i * j + "\t" );
+            }
+            System.out.println();
+        }
+    }
+}
